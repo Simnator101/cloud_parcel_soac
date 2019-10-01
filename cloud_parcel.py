bd75c03d034a1ed61c2d3b8e5b30a0eea914a604
Simnator101 <simnator@gmail.com>
2019-10-01 16:00:34 +0200

    Cloud Humidity: :

--- cloud_parcel.py
+++ cloud_parcel.py
@@ -13,8 +13,10 @@ import sounding as snd
 gval = 9.81 # m / s^2
 Rideal = 8.31446261815324	# J / k / mol
 Rair = 287.058  # J / k / kg
+Rv = 461.5      # J / k / kg
 cpa = 1004.0    # J / kg / K
 cpv = 716.0     # J / kg / K
+Lv = 2.5e6      # J / kg
 
 def trial_lapse_rate(z):
     if 0.0 <= z < 100.0:
@@ -26,10 +28,11 @@ def trial_lapse_rate(z):
 
 
 class CloudParcel(object):
-    def __init__(self, T0=300.0, z0=0.0, w0=0.0):
+    def __init__(self, T0=300.0, z0=0.0, w0=0.0, q0=0.0):
         self.__t0 = T0
         self.__w0 = w0
         self.__z0 = z0
+        self.__q0 = q0
         self.induced_mass = 0.5
         self.storage = None
         
@@ -43,38 +46,103 @@ class CloudParcel(object):
         T = np.full(NT, self.__t0)
         w = np.full(NT, self.__w0)
         z = np.full(NT, self.__z0)
+        q = np.full(NT, self.__q0)
+        l = np.zeros(NT)
+        p = environ.pressure_profile()
         acc = gval / (1 + self.induced_mass)
         
-        def wf(T, z):
-            return acc * (T - environ.sample(z)) / environ.sample(z)
+        def wf(T, z, l):
+            return acc * ((T - environ.sample(z)) / environ.sample(z) - l)
         
-        def Tf(w):
-            return -gval / cpa * w
+        def flux(wv, wl, T, z):
+            wmax = saturation_pressure(T) / np.interp(z, environ.height, p) * Rair / Rv
+            return min(wmax - wv, wl)
+        
+        def Tf(w, wv, wl, T, z):
+            return -gval / cpa * w - Lv / cpa * flux(wv, wl, T, z)
         
         def zf(w):
             return w
         
+        def qf(w, wv, wl, T, z):
+            return flux(w, wl, T, z)
+        
+        def lf(w, wv, wl, T, z):
+            return -flux(w, wl, T, z)
+        
         for i in range(1, NT):
             # K1
-            k1w = dt * wf(T[i-1], z[i-1])
-            k1T = dt * Tf(w[i-1])
+            k1w = dt * wf(T[i-1], z[i-1], l[i-1])
+            k1T = dt * Tf(w[i-1], q[i-1], l[i-1], T[i-1], z[i-1])
             k1z = dt * zf(w[i-1])
+            k1q = dt * qf(w[i-1], q[i-1], l[i-1], T[i-1], z[i-1])
+            k1l = dt * lf(w[i-1], q[i-1], l[i-1], T[i-1], z[i-1])
             # K2
-            k2w = dt * wf(T[i-1] + k1T / 2, z[i-1] + k1z / 2)
-            k2T = dt * Tf(w[i-1] + k1w / 2)
+            k2w = dt * wf(T[i-1] + k1T / 2,
+                          z[i-1] + k1z / 2,
+                          l[i-1] + k1l / 2)
+            k2T = dt * Tf(w[i-1] + k1w / 2,
+                          q[i-1] + k1q / 2,
+                          l[i-1] + k1l / 2,
+                          T[i-1] + k1T / 2,
+                          z[i-1] + k1z / 2)
             k2z = dt * zf(w[i-1] + k1w / 2)
+            k2q = dt * qf(w[i-1] + k1w / 2,
+                          q[i-1] + k1q / 2,
+                          l[i-1] + k1l / 2,
+                          T[i-1] + k1T / 2,
+                          z[i-1] + k1z / 2)
+            k2l = dt * lf(w[i-1] + k1w / 2,
+                          q[i-1] + k1q / 2,
+                          l[i-1] + k1l / 2,
+                          T[i-1] + k1T / 2,
+                          z[i-1] + k1z / 2)
             # K3
-            k3w = dt * wf(T[i-1] + k2T / 2, z[i-1] + k2z / 2)
-            k3T = dt * Tf(w[i-1] + k2w / 2)
+            k3w = dt * wf(T[i-1] + k2T / 2,
+                          z[i-1] + k2z / 2,
+                          l[i-1] + k2l / 2)
+            k3T = dt * Tf(w[i-1] + k2w / 2,
+                          q[i-1] + k2q / 2,
+                          l[i-1] + k2l / 2, 
+                          T[i-1] + k2T / 2,
+                          z[i-1] + k2z / 2)
             k3z = dt * zf(w[i-1] + k2w / 2)
+            k3q = dt * qf(w[i-1] + k2w / 2,
+                          q[i-1] + k2q / 2,
+                          l[i-1] + k2l / 2, 
+                          T[i-1] + k2T / 2,
+                          z[i-1] + k2z / 2)
+            k3l = dt * lf(w[i-1] + k2w / 2,
+                          q[i-1] + k2q / 2,
+                          l[i-1] + k2l / 2, 
+                          T[i-1] + k2T / 2,
+                          z[i-1] + k2z / 2)
             # K4
-            k4w = dt * wf(T[i-1] + k3T, z[i-1] + k3z)
-            k4T = dt * Tf(w[i-1] + k3w)
+            k4w = dt * wf(T[i-1] + k3T,
+                          z[i-1] + k3z,
+                          l[i-1] + k3l)
+            k4T = dt * Tf(w[i-1] + k3w,
+                          q[i-1] + k3q,
+                          l[i-1] + k3l,
+                          T[i-1] + k3T,
+                          z[i-1] + k3z)
             k4z = dt * zf(w[i-1] + k3w)
+            k4q = dt * qf(w[i-1] + k3w,
+                          q[i-1] + k3q,
+                          l[i-1] + k3l,
+                          T[i-1] + k3T,
+                          z[i-1] + k3z)
+            k4l = dt * lf(w[i-1] + k3w,
+                          q[i-1] + k3q,
+                          l[i-1] + k3l,
+                          T[i-1] + k3T,
+                          z[i-1] + k3z)
             # Update
             w[i] = w[i-1] + 1. / 6. * (k1w + 2.0 * (k2w + k3w) + k4w)
             T[i] = T[i-1] + 1. / 6. * (k1T + 2.0 * (k2T + k3T) + k4T)
             z[i] = z[i-1] + 1. / 6. * (k1z + 2.0 * (k2z + k3z) + k4z)
+            q[i] = q[i-1] + 1. / 6. * (k1q + 2.0 * (k2q + k3q) + k4q)
+            l[i] = l[i-1] + 1. / 6. * (k1l + 2.0 * (k2l + k3l) + k4l)
             
         self.storage = (T, w, z)
         return T, w, z
@@ -84,6 +152,6 @@ if __name__ == "__main__":
     snd.from_lapse_rate(trial_lapse_rate, 0, 2e3, 2000)
     
     parcel = CloudParcel(T0 = 301.)
-    T, w, z = parcel.run(1.0, 10000, snd)
-    plt.plot(z)
+    T, w, z = parcel.run(1.0, 6000, snd)
+    plt.plot(T)
     plt.show()
--- sounding.py
+++ sounding.py
@@ -13,8 +13,15 @@ import matplotlib.pyplot as plt
 gval = 9.81 # m / s^2
 Rideal = 8.31446261815324	# J / k / mol
 Rair = 287.058  # J / k / kg
+Rv = 461.5      # J / k / kg
+Lv = 2.5e6      # J / kg
 cpa = 1004.0    # J / kg / K
 cpv = 716.0     # J / kg / K
+
+
+def saturation_pressure(T):
+    T0 = 273.16
+    return 610.78 * np.exp(Lv / Rv / T0 * (T - T0) / T)
     
 
 class Sounding(object):
@@ -32,6 +39,7 @@ class Sounding(object):
             self.__T[i] = self.__T[i-1] + g_rate(zv) * dz
         
     def sample(self, z):
+        #assert(self.__z.max() + 10.0 >= z and self.__z.min() - 10.0 <= z)
         return np.interp(z, self.__z, self.__T)
     
             
@@ -40,6 +48,19 @@ class Sounding(object):
         T = np.array(T, dtype=np.float)
         self.__T = np.r_[self.__T, T] if self.__T is not None else T
         self.__z = np.r_[self.__z, z] if self.__z is not None else z
+        
+    def pressure_profile(self, p0=1e5):
+        assert(self.__z is not None and self.__T is not None)
+        p = np.empty(self.__z.size)
+        p[0] = p0
+        for i in range(1, p.size):
+            p[i] = p0 * np.exp(np.trapz(-gval / Rair / self.__T[:i], self.__z[:i]))
+        return p
+    
+    
+    @property
+    def height(self):
+        return self.__z
         
     def plot(self):
         f, ax = plt.subplots()
@@ -51,11 +72,13 @@ if __name__ == "__main__":
     def trial_lapse_rate(z):
         if 0.0 <= z < 100.0:
             return -20.0e-3
-        elif 100.0 <= z < 800.0:
+        elif 100.0 <= z < 2000.0:
             return -9.8e-3
         else:
             return 20e-3
     
     snd = Sounding(None, None)
-    snd.from_lapse_rate(trial_lapse_rate, 0, 1e3, 1000)
+    snd.from_lapse_rate(trial_lapse_rate, 0, 2e3, 1000)
     snd.plot()
+    snd.sample(1e3)
+    plt.plot(snd.pressure_profile(), snd.height)
