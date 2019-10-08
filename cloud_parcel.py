b42f587da1a2965fa5e93c6264801553c94ef0bf
Simnator101 <simnator@gmail.com>
2019-10-08 12:59:19 +0200

    Expanded vapour<->liquid exchange module

--- cloud_parcel.py
+++ cloud_parcel.py
@@ -30,21 +30,30 @@ def trial_lapse_rate(z):
 
 
 class CloudParcel(object):
-    def __init__(self, T0=300.0, z0=0.0, w0=0.0, q0=0.0, Kmix=1.):
+    def __init__(self, T0=300.0, z0=0.0, w0=0.0, q0=0.0, mix_len=0.0):
         self.__t0 = T0
         self.__w0 = w0
         self.__z0 = z0
         self.__q0 = q0
-        self.__Kmix = Kmix
+        self.__mu = mix_len
         self.induced_mass = 0.5
         self.storage = None
+        self.dt = None
+        
+    @staticmethod
+    def tanh_vapour_flow(wv, wl, wmax, dt):
+        v = -np.tanh(15.0 * (wv / wmax - 1.0)) * 0.2 * dt
+        if v < 0.0:
+            return wv * v
+        return wl * v
         
         
-    def run(self, dt, NT, environ):
+    def run(self, dt, NT, environ, flux_func=None):
         assert(environ is not None)
         assert(NT > 0)
         assert(dt > 0.0)
-        print(self.__t0, self.__w0, self.__z0)
+        #print(self.__t0, self.__w0, self.__z0)
+        self.dt = dt
         
         T = np.full(NT, self.__t0)
         w = np.full(NT, self.__w0)
@@ -54,37 +63,58 @@ class CloudParcel(object):
         p = environ.pressure_profile()
         acc = gval / (1 + self.induced_mass)
         
-        def wf(T, z, l):
-            return acc * ((T - environ.sample(z)) / environ.sample(z) - l)
+        def wf(T, z, l, w):
+            mu_eval = self.__mu
+            if type(mu_eval) is not float:
+               mu_eval = mu_eval(z)            
+            
+            return acc * ((T - environ.sample(z)) / environ.sample(z) - l) -\
+                          mu_eval /(1. + self.induced_mass) * abs(w) * w
         
+        # Different Flux functions
         def flux(wv, wl, T, z):
             wmax = snd.saturation_pressure(T) / np.interp(z, environ.height, p) * Rair / Rv
-            #return min(wmax - wv, wl)
-            if wv/wmax > 1.01:
-                fl = wmax - wv
-            elif wv/wmax < 0.99:
-                fl = min(wmax - wv, wl)
-            else:
-                fl = 0
-            return fl*(1-np.exp(-dt/self.__Kmix))
+            if flux_func is None:
+                fl = 0.0
+                if wv/wmax > 1.05:
+                    fl = wmax - wv
+                elif wv/wmax < 0.95:
+                    fl = min(wmax - wv, wl)
+                return fl#*(1-np.exp(-dt/self.__Kmix))
+            
+            assert callable(flux_func), str(flux_func) + " is not a function"
+            return flux_func(wv, wl, wmax, dt)
+        
         
         def Tf(w, wv, wl, T, z):
-            return -gval / cpa * w - Lv / cpa * flux(wv, wl, T, z)
+            mu_eval = self.__mu
+            if type(mu_eval) is not float:
+               mu_eval = mu_eval(z) 
+            return -gval / cpa * w - Lv / cpa * flux(wv, wl, T, z) - mu_eval *\
+                    (T - environ.sample(z)) * abs(w)
         
         def zf(w):
             return w
         
         def qf(w, wv, wl, T, z):
-            return flux(wv, wl, T, z)
+            mu_eval = self.__mu
+            if type(mu_eval) is not float:
+               mu_eval = mu_eval(z)
+            return flux(wv, wl, T, z) - mu_eval * (wv - environ.sample_q(z)) *  abs(w)
         
         def lf(w, wv, wl, T, z):
-            return -flux(wv, wl, T, z)
+            mu_eval = self.__mu
+            if type(mu_eval) is not float:
+               mu_eval = mu_eval(z)
+            return -flux(wv, wl, T, z) - mu_eval * wl * abs(w)
+        
         
         for i in range(1, NT):
             # K1
             k1w = dt * wf(T[i-1], 
                           z[i-1], 
-                          l[i-1])
+                          l[i-1],
+                          w[i-1])
             k1T = dt * Tf(w[i-1], 
                           q[i-1], 
                           l[i-1], 
@@ -104,7 +134,8 @@ class CloudParcel(object):
             # K2
             k2w = dt * wf(T[i-1] + k1T / 2,
                           z[i-1] + k1z / 2,
-                          l[i-1] + k1l / 2)
+                          l[i-1] + k1l / 2,
+                          w[i-1] + k1w / 2)
             k2T = dt * Tf(w[i-1] + k1w / 2,
                           q[i-1] + k1q / 2,
                           l[i-1] + k1l / 2,
@@ -124,7 +155,8 @@ class CloudParcel(object):
             # K3
             k3w = dt * wf(T[i-1] + k2T / 2,
                           z[i-1] + k2z / 2,
-                          l[i-1] + k2l / 2)
+                          l[i-1] + k2l / 2,
+                          w[i-1] + k2w / 2)
             k3T = dt * Tf(w[i-1] + k2w / 2,
                           q[i-1] + k2q / 2,
                           l[i-1] + k2l / 2, 
@@ -144,7 +176,8 @@ class CloudParcel(object):
             # K4
             k4w = dt * wf(T[i-1] + k3T,
                           z[i-1] + k3z,
-                          l[i-1] + k3l)
+                          l[i-1] + k3l,
+                          w[i-1] + k3w)
             k4T = dt * Tf(w[i-1] + k3w,
                           q[i-1] + k3q,
                           l[i-1] + k3l,
@@ -170,29 +203,87 @@ class CloudParcel(object):
             
         self.storage = (T, w, z, q, l)
         return T, w, z, q, l
+    
+    @property
+    def static_energy(self):
+        return self.storage[0] * cpa + gval * self.storage[2]
+    
+    @property
+    def static_moist_energy(self):
+        return self.static_energy + self.storage[3] * Lv
+    
+# TEST PLOTS
+def test_energy_budget(parcel):
+    f, ax = plt.subplots(1, 2, sharey=True, figsize=(9, 6))
+    t = np.arange(len(parcel.storage[0])) * parcel.dt
+    
+    ax[0].plot(t, parcel.static_energy * 1e-3)
+    ax[0].grid(True)
+    ax[0].set_xlabel("Time (s)")
+    ax[0].set_ylabel("Energy (kJ/kg)")
+    ax[0].set_title("Static Energy")
+    ax[0].set_ylim(parcel.static_energy.min() * 1e-3 - 10,
+                   parcel.static_energy.max() * 1e-3 + 10)
+    
+    ax[1].plot(t, parcel.static_moist_energy * 1e-3)
+    ax[1].grid(True)
+    ax[1].set_xlabel("Time (s)")
+    ax[1].set_title("Static Moist Energy")
+    plt.show()
+    del t
+    
+def test_water_budget(parcel):
+    f, ax = plt.subplots()
+    t = np.arange(len(parcel.storage[0])) * parcel.dt
+    
+    ax.grid(True)
+    ax.set_xlabel("Time (s)")
+    ax.set_ylabel("Water Concentration (g/kg)")
+    ax.set_title("Water Stores")
+    
+    ax.plot(t, parcel.storage[3] * 1e3, label='Water Vapour')
+    ax.plot(t, parcel.storage[4] * 1e3, label='Water Liquid')
+    ax.plot(t, np.sum(parcel.storage[3:], axis=0) * 1e3, label='Water Budget')
+    
+    plt.legend(bbox_to_anchor=(1,1))
+    plt.show()
+    del t
             
 if __name__ == "__main__":
     sounding = snd.Sounding(None, None)
     sounding.from_lapse_rate(trial_lapse_rate, 0, 20e3, 10000)
+    #sounding.from_wyoming_httpd(snd.SoundingRegion.NORTH_AMERICA, 72201, "01102019")
     
-    parcel = CloudParcel(T0 = 301., q0=0.00)
+    parcel = CloudParcel(T0 = sounding.surface_temperature + 1.,
+                         q0 = 0.02,
+                         mix_len=0.0, w0=0.0)
     
-    T, w, z, q, l = parcel.run(1.0, 6000, sounding)
+    T, w, z, q, l = parcel.run(0.5, 6000, sounding, flux_func=CloudParcel.tanh_vapour_flow)
 
-    #p = np.full(T.size, 1e5)
-    #for i in range(1, T.size):
-    #    p[i] = p[0] * np.exp(np.trapz(-gval / Rair / T[:i], z[:i]))
-    plt.plot(z)
+
+    plt.plot(np.arange(6000) * 0.5, z)
     plt.show()
-    #plt.plot(q+l)
-    #plt.ylim([0.,0.0201])
-    #plt.show()
-    #plt.plot(0.5*w**2+gval*z+cpa*T+q*cpv*T-(Rair+q*Rv)*T)
-    #plt.show()
-    #plt.plot(0.5*w**2+gval*z+cpa*T+q*cpv*T-(Rair+q*Rv)*T*(p-p[0])/p)
-    #plt.plot(0.5*w**2)
-    #plt.plot(gval*z)
-    #plt.plot(cpa*T)
-    #plt.plot(q*cpv*T)
-    #plt.plot(-(Rair+q*Rv)*T*(p-p[0])/p)
-    #plt.legend(['total','kin','pot','temp','water','druk'])
+    
+    p = np.full(T.size, 1e5)
+    for i in range(1, T.size):
+        p[i] = p[0] * np.exp(np.trapz(-gval / Rair / T[:i], z[:i]))
+    f, ax = sounding.SkewT_logP(show=False)
+    ax.plot(T, p * 1e-2)
+    plt.show()
+    
+    test_energy_budget(parcel)
+    test_water_budget(parcel)
+    
+    
+#    plt.plot(q+l)
+#    plt.ylim([0.,0.0201])
+#    plt.show()
+#    plt.plot(0.5*w**2+gval*z+cpa*T+q*cpv*T-(Rair+q*Rv)*T)
+#    plt.show()
+#    plt.plot(0.5*w**2+gval*z+cpa*T+q*cpv*T-(Rair+q*Rv)*T*(p-p[0])/p)
+#    plt.plot(0.5*w**2)
+#    plt.plot(gval*z)
+#    plt.plot(cpa*T)
+#    plt.plot(q*cpv*T)
+#    plt.plot(-(Rair+q*Rv)*T*(p-p[0])/p)
+#    plt.legend(['total','kin','pot','temp','water','druk'])
